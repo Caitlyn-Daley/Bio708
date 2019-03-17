dab9f46ea25bd90bc24a41bb943710876eaa75bc
Ben Bolker <bbolker@gmail.com>
2019-03-17 11:46:10 -0400

    BMB comments

--- Generalized_linear_model.R
+++ Generalized_linear_model.R
@@ -1,16 +1,33 @@
 library(tidyr)
-Data <- read.csv("WingData.csv")
+wings <- read.csv("WingData.csv")
 summary(Data)
-Data$DGRP <- factor(Data$DGRP)
-Data$Mutant <- relevel(Data$Mutant, "OREw")
+wings$DGRP <- factor(wings$DGRP)
+wings$Mutant <- relevel(wings$Mutant, "OREw")
 ##releveling so we have the control as the reference level to compare to 
-str(Data)
+str(wings)
 ##all of my variables in my dataset are continuous, so to make a GLM make sense,
 ##I will use a gamma distribution with a log/link. 
 ##This general linear model accounts for the effects of each mutant and each DGRP
-generallm <- glm(Area_mmsq ~ Mutant + DGRP + Mutant:DGRP, wings, family= Gamma(link="log"))
+generallm <- glm(Area_mmsq ~ Mutant + DGRP + Mutant:DGRP,
+                 wings, family= Gamma(link="log"))
+
+## BMB: NOT reproducible.  I had to call Data "wings" instead?
 summary(generallm)
 plot(generallm)
-##the linear model appears to be a better fit than the genrealized linear model
-##ideally for this dataset you would use a mixed model rather than a genrealized linear model to account for fixed 
+##the linear model appears to be a better fit than the generalized linear model
+##ideally for this dataset you would use a mixed model rather than a generalized linear model to account for fixed 
 ##effects (i.e Mutant, Sex, Temperature) and random effects (DGRP, Block)
+
+## BMB: true, at least in terms of the Q-Q plot.
+## it would 
+
+loglm <- lm(log(Area_mmsq) ~ Mutant + DGRP + Mutant:DGRP,
+                 wings)
+AIC(loglm)+sum(2*log(wings$Area_mmsq))
+
+reglm <- lm(Area_mmsq ~ Mutant + DGRP + Mutant:DGRP,
+                 wings)
+AIC(reglm)
+AIC(generallm)
+
+## https://stats.stackexchange.com/questions/61332/comparing-aic-of-a-model-and-its-log-transformed-version
--- Linear_Models_Assignment.R
+++ Linear_Models_Assignment.R
@@ -11,14 +11,40 @@ str(Data)
 ##This model accounts for the effects of each mutant and each DGRP
 mutantgenotypelm <- lm(Area_mmsq ~ Mutant + DGRP + Mutant:DGRP, data = Data,
                        contrasts = list(DGRP = contr.sum))
+## BMB: could use Mutant*DGRP equivalently (but this is OK too)
 plot(mutantgenotypelm)
+
+## BMB: what do you conclude about the diagnostics??
+
 summary(mutantgenotypelm)
-##The drop1 fuction tells us the overall effect of each mutant indivudally
-##opposed to the mutants as a whole 
+##The drop1 function tells us the overall effect of each mutant individually
+##opposed to the mutants as a whole
+## BMB: isn't it kind of the opposite? drop1() gives *overall* p-value
+## -- also, it gives the p-value for the *interaction*
 drop1(mutantgenotypelm, test = "F")
+
 ## you can use this dwplot to look at the interactions between the mutant and 
 ##the DGRP and you can write more code to rank DGRP in terms of their effect on the mutant
 dwplot(mutantgenotypelm)
+
+## BMB how about:
+cc <- broom::tidy(mutantgenotypelm,conf.int=TRUE)
+library(cowplot)
+cc_main <- droplevels(subset(cc, term!="(Intercept)" & !grepl(":",term)))
+cc_main$term <- reorder(cc_main$term, cc_main$estimate)
+cc_inter <- droplevels(subset(cc, grepl(":",term)))
+cc_inter$term <- reorder(cc_inter$term, cc_inter$estimate)
+
+gg_main <- ggplot(cc_main, aes(term, estimate))+
+    geom_pointrange(aes(ymin=conf.low,ymax=conf.high))+
+    scale_y_continuous(limits=c(-1,1))+
+    coord_flip()
+
+gg_inter <- gg_main %+% cc_inter
+pdf("linmodels.pdf",height=25,width=10)
+plot_grid(gg_main,gg_inter)
+dev.off()
+
 OREwemmeans <- emmeans(mutantgenotypelm, "DGRP",
                          at = list(Mutant = "OREw"))
 sde3emmeans <- emmeans(mutantgenotypelm, "DGRP",
@@ -35,4 +61,5 @@ plot(sd1emmeans)
 plot(sde3emmeans)
 ##sd[ETX4]
 plot(sdETX4emmeans)
-     
+## BMB: can you explain what all of these mean?
+## score: 2
